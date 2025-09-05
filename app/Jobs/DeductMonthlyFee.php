50d58d66615914ebf24de3aedeb55747e01bca18
Godswill Okpanku <godswillokpanku@gmail.com>
2025-09-05 05:25:39 -0700

    updated

--- app/Jobs/DeductMonthlyFee.php
+++ app/Jobs/DeductMonthlyFee.php
@@ -3,15 +3,16 @@
 namespace App\Jobs;
 
 
-use App\Models\VirtualAccount;
-use App\Models\Transaction;
 use App\Models\MonthlyFee;
+use App\Models\Transaction;
 use Illuminate\Bus\Queueable;
+use App\Models\DisburseDetail;
+use App\Models\VirtualAccount;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Queue\SerializesModels;
-use Illuminate\Support\Facades\Log;
 
 class DeductMonthlyFee implements ShouldQueue
 {
@@ -19,11 +20,15 @@ class DeductMonthlyFee implements ShouldQueue
 
     protected $virtualAccount;
     protected $monthlyFee;
+    protected $mainAcctName;
+    protected $mainAcctNumber;
 
     public function __construct(VirtualAccount $virtualAccount, $amount)
     {
         $this->virtualAccount = $virtualAccount;
         $this->monthlyFee = $amount;
+        $this->mainAcctName = config('monnify.main_account_name');
+        $this->mainAcctNumber = config('monnify.main_account_number');
     }
 
     public function handle(): void
@@ -46,8 +51,8 @@ class DeductMonthlyFee implements ShouldQueue
 
             $virtual_account->decrement('balance', $deduction);
             $virtual_account->decrement('arrears', $deduction);
-            
-            Transaction::create([
+
+            $transaction = Transaction::create([
                 'user_id' => $virtual_account->user_id,
                 'virtual_account_id' => $virtual_account->id,
                 'amount' => $deduction,
@@ -56,6 +61,14 @@ class DeductMonthlyFee implements ShouldQueue
                 'narration' => 'Monthly Fee Deduction',
                 'is_completed' => $balance >= $arrears ? 'PAID' : 'PARTIALLY',
             ]);
-        } 
+
+            $disburse_detail = new DisburseDetail;
+            $disburse_detail->transaction_id = $transaction->id;
+            $disburse_detail->total_fee = 0.00;
+            $disburse_detail->destination_bank_name = $this->mainAcctName;
+            $disburse_detail->destination_account_number = $this->mainAcctNumber;
+            $disburse_detail->destination_bank_code = 'UNKNOWN';
+            $disburse_detail->save();
+        }
     }
 }
